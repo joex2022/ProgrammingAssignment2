64c7697dbc3ac0bd85d0ecfa74be25e547b72439
joex2022 <108899005+joex2022@users.noreply.github.com>
2022-07-21 15:53:26 -0400

    Add files via upload

---
+++ r_programming_wk3.R
@@ -0,0 +1,33 @@
+##makeCacheMatrix computes the inverse of input matrix and save it in cache
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+#cachesolve checks the cache for the matrix's inverse first before computes it
+cacheSolve <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
+
+
+##test both macros
+c=rbind(c(1, -0.3), c(-0.3, 1))
+tstm=makeCacheMatrix(c)
+cacheSolve(tstm)
